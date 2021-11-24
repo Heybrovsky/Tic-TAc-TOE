24232b232a5cc84ea9abed44df9e1a0035b347c1
Marceli <marcelikusmierski@gmail.com>
2021-11-24 13:41:18 +0100

    TicTacToe

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public int index;
+    public Mark mark;
+    public bool isMarked;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        index = transform.GetSiblingIndex();
+        mark = Mark.None;
+        isMarked = false;
+    }
+
+    public void SetAsMarked (Sprite sprite, Mark mark, Color color)
+    {
+        isMarked = true;
+        this.mark = mark;
+
+        spriteRenderer.color = color;
+        spriteRenderer.sprite = sprite;
+
+        GetComponent<CircleCollider2D>().enabled = false;
+    }
+}
